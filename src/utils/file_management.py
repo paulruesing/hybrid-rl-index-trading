8b43d0022f1df5f2d60a48c6780648bfa509cd4f
paulrusing <paul.ruesing@rwth-aachen.de>
2025-10-14 15:17:37 +0200

    intelligent config txt file mgmt

--- src/utils/file_management.py
+++ src/utils/file_management.py
@@ -92,3 +92,55 @@ def most_recent_file(directory: Union[Path, str], suffix_to_consider: str = None
         raise NotADirectoryError("Provided path is not a directory (i.e. contains dots)!")
 
 
+class TxtConfig:
+    def __init__(self,
+                 txt_file_path: Union[Path, str],):
+        self.txt_file_path = txt_file_path
+
+    @property
+    def settings_dict(self) -> dict:
+        temp_dict = {}
+        with open(self.txt_file_path, "r") as file:
+            for line in file:
+                line.strip()
+
+                # read entry and sanity check:
+                entry = line.split(' --- ')
+                if len(entry) != 2:
+                    raise ValueError("Provided file may only contain lines structured as 'PROPERTY_NAME --- ENTRY'!")
+
+                # save entry:
+                temp_dict[entry[0].strip()] = entry[1].strip()
+        return temp_dict
+
+    def change_entry(self, entry, new_entry):
+        temp_dict = self.settings_dict
+        temp_dict[entry] = new_entry
+        self._set_dict_to_file(temp_dict)
+
+    def _set_dict_to_file(self, new_dict):
+        # overwrite txt file with current dictionary content
+        with open(self.txt_file_path, "w") as file:
+            for key, value in new_dict.items():
+                if isinstance(value, list):
+                    value = ", ".join(value)  # list formatting (can be retrieved via get_as_type)
+                file.write(f"{str(key)} --- {str(value)}\n")
+
+    def get_as_type(self, key, value_type: Literal["int", "float", "float_list", "str_list", "list", "bool", "str"]):
+        value = self.settings_dict[key]  # retrieve value
+
+        # format and return value:
+        if value_type == "int": return int(value)
+        elif value_type == "float": return float(value)
+        elif value_type == "bool": return bool(value)
+        elif value_type == "str": return str(value)
+        elif value_type == "float_list":
+            entries = value.split(', ')
+            if len(entries) <= 1: raise ValueError("List entries need to be formatted as 'ENTRY_1, ENTRY_2, ENTRY_3, ...'!")
+            return [float(e) for e in entries]
+        elif value_type == "str_list" or value_type == "list":
+            entries = value.split(', ')
+            if len(entries) <= 1: raise ValueError("List entries need to be formatted as 'ENTRY_1, ENTRY_2, ENTRY_3, ...'!")
+            return entries
+        else:
+            raise ValueError(f"Provided value type '{value_type}' is not recognized!")
